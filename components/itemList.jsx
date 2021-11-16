d9d6bca10b694629d53aaac057e20bc61b172a2c
Frédéric Lang <f.lang@hotmail.fr>
2021-11-16 18:28:46 +0100

    drag n drop

--- components/itemList.jsx
+++ components/itemList.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import Delete from "./Delete";
 import Edit from "./Edit";
 import Add from "./Add";
-import Input from "./Input";
+import Input from "./Input"
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const ItemList = () => {
   const [items, setItems] = useState([
-    [true,"Go to the Swimming Pool 🏊"], 
-    [false, "Watch a movie 🎬"], 
-    [false, "Check your agenda 📅"]
+    [true,"Go to the Swimming Pool 🏊", "1"], 
+    [false, "Watch a movie 🎬", "2"], 
+    [false, "Check your agenda 📅", "3"]
   ]);
   const [inputOpen, setInputOpen] = useState(false);
   const [itemToEdit, setItemToEdit] = useState(null); 
+  const [counter, setCounter] = useState(4);
 
   function deleteItem(i) {
     setItems( items => {
@@ -23,7 +26,8 @@ const ItemList = () => {
   function addItem(item) {
     setItems(items => {
       const newItems = [...items];
-      newItems.push([false, item]);
+      newItems.push([false, item, String(counter)]);
+      setCounter(counter => counter + 1)
       return newItems;
     });
   }
@@ -43,36 +47,70 @@ const ItemList = () => {
     })
   }
 
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    const myitems = Array.from(items);
+    const [reorderedItem] = myitems.splice(result.source.index, 1);
+    myitems.splice(result.destination.index, 0, reorderedItem);
+
+    setItems(myitems);
+  }
+
   return (
-    <div className="flex flex-col gap-y-2 w-full sm:w-2/3 md:w-1/2 max-w-sm rounded-lg p-2 shadow-md bg-gray-100 text-left">
-      {items.map((item, i) => (
-        i == itemToEdit ?
-        <Input addReminder={(reminder) => {setItemToEdit(null); editItem(i, reminder)}} defaultAction="Edit" initialValue={item[1]} />
-        :
-        <div
-          key={i}
-          className="border bg-gray-200 rounded flex flex-row items-center p-1 justify-between "
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+    <Droppable droppableId="characters">
+      { (provided) => (
+      <div 
+        className="flex flex-col gap-y-2 w-full sm:w-2/3 md:w-1/2 max-w-sm rounded-lg p-2 shadow-md bg-gray-100 text-left characters"
+        {...provided.droppableProps} 
+        ref={provided.innerRef}
         >
-          <label className="flex items-center justify-start space-x-3 flex-auto overflow-auto">
-            <input
-              type="checkbox"
-              name="checked-demo"
-              onChange={handleCheck(i)}
-              checked={item[0]}
-              className="form-tick appearance-none flex-initial bg-white bg-check h-6 w-24px min-w-6 border border-gray-300 rounded-md checked:bg-blue-500 checked:border-transparent focus:outline-none"
-            />
-            <span className="font-light truncate max-h-16">{item[1]}</span>
-          </label>
-          <span className="flex flex-row">
-            <Edit onClick={() => setItemToEdit(i)} />
-            <Delete onClick={() => deleteItem(i)} />
-          </span>
-        </div>
-      ))}
+        {items.map(([checked, desc, id], index) => (
+          <Draggable key={id} draggableId={id} index={index}>
+          {(provided) => (
+            <div
+              ref={provided.innerRef} 
+              {...provided.draggableProps} 
+              {...provided.dragHandleProps}
+              style={provided.draggableProps.style}
+              >
+
+            {index == itemToEdit ?
+              <Input addReminder={(reminder) => {setItemToEdit(null); editItem(index, reminder)}} defaultAction="Edit" initialValue={desc} />
+            : 
+              <div
+                key={id}
+                className="border bg-gray-200 rounded flex flex-row items-center p-1 justify-between select-none"
+              >
+                <label className="flex items-center justify-start space-x-3 overflow-auto">
+                  <input
+                    type="checkbox"
+                    name="checked-demo"
+                    onChange={handleCheck(index)}
+                    checked={checked}
+                    className="form-tick appearance-none flex-initial bg-white bg-check h-6 w-24px min-w-6 border border-gray-300 rounded-md checked:bg-blue-500 checked:border-transparent focus:outline-none"
+                  />
+                </label> 
+                <span className="font-light truncate max-h-16">
+                  {desc}
+                </span>
+                <span className="flex flex-row">
+                  <Edit onClick={() => setItemToEdit(index)} />
+                  <Delete onClick={() => deleteItem(index)} />
+                </span>
+              </div>}
+            </div>
+          )}
+          </Draggable>
+        ))}
+      {provided.placeholder}
       {inputOpen ? 
         <Input addReminder={(reminder) => {setInputOpen(false); addItem(reminder)}} /> : 
         <Add onClick={() => setInputOpen(true)} />}
-    </div>
+      </div>
+      )}
+    </Droppable>
+    </DragDropContext>
   );
 };
 
