05e21faec369d733876ce602cd9c3493f9009fb7
fredlnl <fredlnl@users.noreply.github.com>
2023-04-05 19:07:31 +0000

    deploy: eca5b0fc54e5d3e0a677933a77d48ed46eb13848

---
+++ _next/static/UBdTfSZkzbXvBltrHTZYR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/e10ed010-178ef6dd2637270a.js","static/chunks/161-6211e45522a7da6a.js","static/chunks/pages/index-0b5dfad3bb8918ad.js"],"/_error":["static/chunks/pages/_error-a12d0fec618c6e85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
